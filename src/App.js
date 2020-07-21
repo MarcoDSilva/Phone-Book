7e68bafd735a4341094a380e6c8c6d2320663b52
MarcoDSilva <38049196+MarcoDSilva@users.noreply.github.com>
2020-07-21 18:25:06 +0100

    exe 2.11

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Filter from "./Components/Filter.js";
 import PersonForm from "./Components/PersonForm.js";
 import Persons from "./Components/Persons.js";
+import axios from 'axios'
 
 const App = () => {
   // array of people and method to update it
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "91999999999" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newPhone, setNewPhone] = useState("");
   const [newFilter, setNewFilter] = useState("");
+
+  // fetching api data 
+  const hook = () => {
+    axios.get('http://localhost:3001/persons').then(res => {
+      console.log('promise fulfilled')
+      setPersons(res.data)
+    })
+  }
+
+  // aplying the data fetched with the hook
+  useEffect(hook, [])
 
   const resetForm = () => {
     setNewName("");
